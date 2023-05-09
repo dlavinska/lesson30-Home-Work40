21610caea7e632a3fb3bbbec1d9f77bba7bc4e0e
Dariia Lavinska <d.lavinska@gmail.com>
2023-05-09 15:16:42 +0300

    add hw40

--- src/App.js
+++ src/App.js
@@ -1,25 +1,90 @@
 import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import articles from './data/articles.json'
+
+const AsideNav = () => {
+  return (
+    <aside className="aside-nav">
+      <Navigation />
+    </aside>
+  );
+}
+
+const Header = () => {
+  return (
+    <div className="header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <h3>This is my first React-app</h3>
+    </div>
+  );
+}
+
+const Main = () => {
+  return (
+    <div className="main">
+      <h1>Main Section</h1>
+      {articles.map(item =>
+        <section key={item.id}>
+          <h2>{item.title}</h2>
+          <div>{item.body}</div>
+        </section>)}
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Main />
+      <AsideNav/>
     </div>
   );
 }
 
 export default App;
+
+class Navigation extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      data: articles
+    }
+  }
+
+  render() {
+    return (
+      <nav>
+        <ul>
+          {this.state.data.map((item, index) => {
+
+            const number = index + 1;
+
+            return (
+              <ListItems
+                key={item.id}
+                number={number}
+                href={item.href}
+                title = {item.title}
+            />
+            )
+          })}
+        </ul>
+      </nav>
+    ) 
+  }
+
+}
+
+function ListItems(props) {
+  return (
+    <li>
+      <span>
+        {props.number} {"article: "}
+      </span>
+      <a href={props.href} target="_blank" rel="noopener noreferrer">{props.title}</a>
+    </li>
+  );
+}
